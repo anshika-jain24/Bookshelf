b039ec408a1ba079f291dca401daea2602d91c5c
Anshika Jain <jainanshika193@gmail.com>
2021-12-10 14:35:51 +0530

    No Book found

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import {
 } from "react-router-dom";
 import Bookshelf from "./components/Bookshelf";
 import ResponsiveDrawer from "./components/Sidebar";
+import bookdata from './sample data/bookdata.json'
 
 const BookRender = () => {
     const {book} = useParams();
+
+    const found = bookdata.filter(item => item.title===book);
+
+    if(found.length === 0)
+    {
+      console.log('No such Book');
+      return(<Bookshelf/>)
+    }
 
     return (
       <ResponsiveDrawer book={book} />
